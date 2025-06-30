31c27a62da1f9c6533e0081b218e9b1604adf499
Meenakshi5009 <meenakshim9005@gmail.com>
2025-06-30 13:03:38 +0530

    Add files via upload

---
+++ BACKEND-1/server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+
+const app = express();
+
+// Must be before routes!
+app.use(cors());
+app.use(express.json()); // Required for parsing JSON body
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
+//Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname,"uploads")));
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
