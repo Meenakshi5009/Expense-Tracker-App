1d3aa4f4954daed5700bf7eec85009a4f545acec
Meenakshi5009 <meenakshim9005@gmail.com>
2025-07-04 21:19:22 +0530

    Update AuthLayout.jsx

--- FRONTEND/src/components/layouts/AuthLayout.jsx
+++ FRONTEND/src/components/layouts/AuthLayout.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import LOGO from "../../assets/images/logo2.png";
-import RIGHT_IMAGE from "../../assets/images/illustration.jpeg";
+import LOGO from "../../assets/images/xpenso.png";
+import RIGHT_IMAGE from "../../assets/images/rightxp.png";
 
 const AuthLayout = ({ children }) => {
     return (
         <div className="w-screen h-screen flex flex-row bg-neutral-200">
             {/* Left Panel */}
-            <div className="w-full md:w-[60vw] px-10 md:px-16 pt-8 pb-12 flex flex-col">
-                <div className="flex flex-col items-start mb-10">
+            <div className="relative w-full md:w-[60vw] px-10 md:px-16 pt-8 pb-12 flex flex-col">
+                <div className="absolute top-6 left-6">
                     <img
                         src={LOGO}
                         alt="Xpenso Logo"
-                        className="w-[50px] h-[50px] object-contain bg-transparent"
+                        className="w-[60px] h-[60px] object-contain bg-transparent"
                     />
-                    <h2 className="-mt-1 text-[11px] font-bold text-gray-900">Xpenso</h2>
+                    {/* <h2 className="-mt-1 text-[11px] font-bold text-gray-900">Xpenso</h2> */}
                 </div>
 
                 <div className="flex-grow flex items-center justify-center">
@@ -22,16 +22,16 @@ const AuthLayout = ({ children }) => {
             </div>
 
             {/* Right Panel */}
-            <div className="hidden md:flex w-[40vw] h-full bg-transparent items-center justify-center px-8 py-12">
-                <div className="w-full h-[95%] bg-white rounded-3xl shadow-[4px_4px_10px_rgba(168,85,247,0.2)] border border-violet-500 p-6 flex flex-col items-center justify-center text-center">
-                    <h1 className="text-4xl font-extrabold text-black">Xpenso.</h1>
-                    <p className="text-sm text-primary font-semibold mt-2 uppercase tracking-wide">
+            <div className="hidden md:flex flex-1 items-center justify-center p-8 bg-gray-150">
+                <div className="bg-white rounded-[20px] shadow-[3px_3px_2px_rgba(150,40,300,0.70)] p-8">
+                    {/* <h1 className="text-4xl font-extrabold text-black">Xpenso.</h1> */}
+                    {/* <p className="text-sm text-primary font-semibold mt-2 uppercase tracking-wide">
                         Your All In One Budget Tracker
-                    </p>
+                    </p> */}
                     <img
                         src={RIGHT_IMAGE}
                         alt="Finance Illustration"
-                        className="mt-10 max-w-full h-auto object-contain"
+                        className="w-[600px] h-[640px] object-cover"
                     />
                 </div>
             </div>
@@ -40,4 +40,3 @@ const AuthLayout = ({ children }) => {
 };
 
 export default AuthLayout;
-
