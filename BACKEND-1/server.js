15a0541ed51dea9be6349c4b502bac852889cc7c
Meenakshi5009 <meenakshim9005@gmail.com>
2025-07-05 00:51:50 +0530

    Update server.js

--- BACKEND-1/server.js
+++ BACKEND-1/server.js
@@ -3,32 +3,26 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-
 const authRoutes = require("./routes/authRoutes");
-const expenseRoutes = require("./routes/expenseRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-// Middlewares
+// Must be before routes!
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Required for parsing JSON body
 
-// Connect DB
 connectDB();
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("API is running ");
-});
-
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+//Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname,"uploads")));
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -36,6 +30,6 @@ app.get('/{*any}', (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
-// Start Server
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
