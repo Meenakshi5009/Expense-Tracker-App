5e3881e230635e8ee79fe6fced1dc7cb647b8275
Meenakshi5009 <meenakshim9005@gmail.com>
2025-07-05 09:27:14 +0530

    Update server.js

--- BACKEND-1/server.js
+++ BACKEND-1/server.js
@@ -31,5 +31,5 @@ app.get("*", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
